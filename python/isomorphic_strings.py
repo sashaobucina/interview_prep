feec8baa2230082168befc5f2fc84b5eaf0b6387
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-26 23:39:56 -0400

    Add isomorphic strings solution (Easy)

---
+++ python/isomorphic_strings.py
@@ -0,0 +1,25 @@
+"""
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order
+of characters. No two characters may map to the same character but a character may map to itself.
+"""
+def is_isomorphic(s: str, t: str):
+  if (len(s) != len(t)):
+    return False
+  mapping = {}
+  char_set = set()
+  for i in range(len(s)):
+    if (s[i] not in mapping):
+      if t[i] in char_set:
+        return False
+      mapping[s[i]] = t[i]
+      char_set.add(t[i])
+    else:
+      if mapping[s[i]] != t[i]:
+        return False
+  print(mapping)
+  return True
+
+if __name__ == "__main__":
+  print(is_isomorphic("ab", "aa"))
