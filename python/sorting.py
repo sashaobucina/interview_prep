1c9a1697acf0005727db2ead43b0f1707ffee17e
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-27 21:07:18 -0400

    Add descriptions to sorting algorithms

--- python/sorting.py
+++ python/sorting.py
@@ -1,12 +1,17 @@
 """ Various O(n^2) sorting algorithms """
 
+"""
+Start from beginning and bubble up elements to end.
+"""
 def bubble_sort(arr: list) -> None:
   n = len(arr)
   for i in range(n):
     for j in range(n - i - 1):
       if arr[j] > arr[j+1]:
         arr[j], arr[j + 1] = arr[j+1], arr[j]
-
+"""
+Sort as if you were sorting cards.
+"""
 def insertion_sort(arr: list) -> None:
   n = len(arr)
   for i in range(1, n):
@@ -17,6 +22,9 @@ def insertion_sort(arr: list) -> None:
       j -= 1
     arr[j+1] = key
 
+"""
+Take the minimum element, and place it at the beginning of the sorted section.
+"""
 def selection_sort(arr: list) -> None:
   n = len(arr)
   for i in range(n):
