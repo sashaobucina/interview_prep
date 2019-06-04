bcd83c1dc914ba2b9d6395d3b58b3559c6dc3e5b
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-04 19:42:24 -0400

    Add mergesort algorithm (python)

--- python/sorting.py
+++ python/sorting.py
@@ -34,7 +34,40 @@ def selection_sort(arr: list) -> None:
         min_idx = j
     arr[min_idx], arr[i] = arr[i], arr[min_idx]
 
+""" Various O(nlogn) sorting algorithms """
+
+def mergesort(arr: list) -> None:
+  if len(arr) > 1:
+    mid = len(arr) // 2
+    l = arr[:mid]
+    r = arr[mid:]
+
+    # Merge the left and right subarrays
+    mergesort(l)
+    mergesort(r)
+
+    # Merge the sorted subarrays together
+    i = j = k = 0
+    while i < len(l) and j < len(r):
+      if l[i] < r[j]:
+        arr[k] = l[i]
+        i += 1
+      else:
+        arr[k] = r[j]
+        j += 1
+      k += 1
+
+    while i < len(l):
+      arr[k] = l[i]
+      i += 1
+      k += 1
+    
+    while j < len(r):
+      arr[k] = r[j]
+      j += 1
+      k += 1
+
 if __name__ == "__main__":
-  arr = [11, 5, 4, 6, 2]
-  selection_sort(arr)
+  arr = [11, 5, 4, 6, 12]
+  mergesort(arr)
   print(arr)
