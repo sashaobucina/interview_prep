28b0dbc74d62114f87578b0b7048f8a5e9cf3b2a
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-05 00:46:13 -0400

    Roman to integer solution

---
+++ python/roman_to_int.py
@@ -0,0 +1,32 @@
+"""
+Note that input is guaranteed to be between 1 and 3999
+"""
+def roman_to_int(s: str) -> int:
+  roman_to_numeral = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  }
+
+  res = i = 0
+  while i < len(s):
+    s1 = roman_to_numeral[s[i]]
+    if i + 1 < len(s):
+      s2 = roman_to_numeral[s[i+1]]
+      if s1 < s2:
+        res += s2 - s1
+        i += 2
+      else:
+        res = res + s1
+        i += 1
+    else:
+      res = res + s1
+      i += 1
+  return res
+
+if __name__ == "__main__":
+  print(roman_to_int('CMI'))
