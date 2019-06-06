428906e64b6ecdcbae764a682b653a32085b97d0
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-06 18:10:35 -0400

    Solution to check if binary trees are same

--- python/binary_tree.py
+++ python/binary_tree.py
@@ -4,6 +4,15 @@ class TreeNode:
     self.left = None
     self.right = None
 
+def isSameTree(p: TreeNode, q: TreeNode) -> bool:
+  # compare current nodes
+  if not p and not q:
+    return True
+  if (p and not q) or (q and not p) or (p.val != q.val):
+    return False
+
+  return isSameTree(p.right, q.right) and isSameTree(p.left, q.left)
+
 def isBalanced(root: TreeNode) -> bool:
   return isBalancedHelper(root) > -1
 
@@ -23,3 +32,8 @@ def isBalancedHelper(root: TreeNode) -> int:
     return -1
 
   return 1 + max(lh, rh)
+
+if __name__ == "__main__":
+  t1 = TreeNode(1)
+  t2 = TreeNode(1)
+  print(isSameTree(t1, t2))
