af88e68c230350ad5edc733a1f3ba53a6e646bbe
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-07 16:38:02 -0400

    Pascal's triangle DP solution

--- python/pascal_triangle.py
+++ python/pascal_triangle.py
@@ -16,5 +16,18 @@ def binomialCoeff(n: int, k: int) -> int:
     res = res // (i+1)
   return res
 
+"""
+Time Complexity: O(numRows^2)
+"""
+def pascalsTriangleDP(numRows: int) -> list:
+  triangle = []
+  for rowNum in range(numRows):
+    row = [None for _ in range(rowNum+1)]
+    row[0], row[-1] = 1, 1
+    for j in range(1, len(row) - 1):
+      row[j] = triangle[rowNum - 1][j-1] + triangle[rowNum - 1][j]
+    triangle.append(row)
+  return triangle
+
 if __name__ == "__main__":
-  print(pascalsTriangle(6))
+  print(pascalsTriangleDP(6))
