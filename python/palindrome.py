dd280626da0a17ab60fa29d725749c4d63b9bfe8
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-07 20:06:23 -0400

    Is valid palindrome for string (python)

--- python/palindrome.py
+++ python/palindrome.py
@@ -8,8 +8,25 @@ def is_palindrome(num: int) -> bool:
     reverted_num = (reverted_num * 10) + (num % 10)
     num = num // 10
 
-  print(reverted_num, num)
   return num == reverted_num or num == reverted_num // 10
+
+def isPalindrome(s: str) -> bool:
+  n = len(s) - 1
+  i = 0
+  while i < n:
+    if not s[i].isalnum():
+      i += 1
+      continue
+    if not s[n].isalnum():
+      n -= 1
+      continue
+    if s[i].lower() != s[n].lower():
+      print(s[i], s[n])
+      return False
+    i += 1
+    n -= 1
+  return True
 
 if __name__ == "__main__":
   print(is_palindrome(123))
+  print(isPalindrome("A man, a plan, a canal: Panama"))
