1b69b29d411704e7b9eed29f0263aa4108901c03
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-09 15:59:59 -0400

    Add 2Sum II solution

--- python/two_sum.py
+++ python/two_sum.py
@@ -7,7 +7,24 @@ def two_sum(nums: list, target: int) -> list:
     s[nums[i]] = i
   return None
 
+"""
+Given an array of integers that is already sorted in ascending order, 
+find two numbers such that they add up to a specific target number.
+"""
+def two_sum_sorted(nums: list, target: int) -> list:
+  lo, hi = 0, len(nums) - 1
+  while lo < hi:
+    sum = nums[lo] + nums[hi]
+    if sum == target:
+      return [lo, hi]
+    if sum < target:
+      lo += 1
+    else:
+      hi -= 1
+  return None
+
 
 if __name__ == "__main__":
   nums = [2, 7, 11, 15]
   print(two_sum(nums, 13))
+  print(two_sum_sorted(nums, 18))
