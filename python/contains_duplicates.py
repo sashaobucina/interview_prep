b8b5e8f07af0a19cdff2eb4e8229ca4bae58f9ab
Sasha Obucina <sasha.obucina@gmail.com>
2019-06-24 22:56:38 -0400

    Solution for contains duplicate problems

---
+++ python/contains_duplicates.py
@@ -0,0 +1,22 @@
+""" Returns true if duplicate present, false otherwise. """
+def contains_duplicate(nums: list) -> bool:
+  s = set(nums)
+  return len(nums) != len(s)
+
+"""
+TODO
+"""
+def contains_duplicate_II(nums: list, k: int) -> bool:
+  tracker = {}
+  for i in range(len(nums)):
+    if nums[i] in tracker and abs(i - tracker[nums[i]]) <= k:
+      return True
+    tracker[nums[i]] = i
+  return False
+
+if __name__ == "__main__":
+  arr = [1, 2, 3, 1]
+  print(contains_duplicate(arr))
+
+  arr2 = [1, 2, 3, 1, 4, 1]
+  print(contains_duplicate_II(arr2, 10))
