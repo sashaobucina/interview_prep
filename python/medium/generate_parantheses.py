63bd9293e9dd57bd2039277448134be685ea364e
Sasha Obucina <sasha.obucina@gmail.com>
2019-07-07 14:08:00 +0200

    Add backtracking solution to generate all possible sets of valid parantheses

---
+++ python/medium/generate_parantheses.py
@@ -0,0 +1,20 @@
+"""
+Solution uses backtracking. Only add a valid sequence by keeping track of left and
+right parantheses.
+"""
+def generateParantheses(n: int) -> list:
+  ans = []
+  def backtrack(S = '', left = 0, right = 0):
+    if len(S) == 2 * n:
+      ans.append(S)
+      return
+    if left < n:
+      backtrack(S + '(', left + 1, right)
+    if right < left:
+      backtrack(S + ')', left, right + 1)
+  backtrack()
+  return ans
+
+if __name__ == "__main__":
+  print(generateParantheses(4))
+
