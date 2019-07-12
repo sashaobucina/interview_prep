c83390ed88e0f115d7b9302732cdc6d7e4b6aa43
Sasha Obucina <sasha.obucina@gmail.com>
2019-07-12 17:34:40 +0200

    Add DP and recursive solution for coin change problem

---
+++ python/medium/coin_change.py
@@ -0,0 +1,38 @@
+"""
+Find the number of unique ways to pay using an infinite set of given coins.
+"""
+def waysToPay(coins: set, n: int, startCoin: int) -> int:
+  if n == 0:
+    return 1
+  if n < 0:
+    return 0
+
+  nCoins = 0
+  for coin in range(startCoin, len(coins)):
+    nCoins += waysToPay(coins, n - coins[coin], coin)
+  return nCoins
+
+"""
+Dynamic Programming
+"""
+def waysToPayDP(coins: set, n: int):
+  m = len(coins)
+  table = [[0 for x in range(m)] for y in range(n + 1)]
+
+  for i in range(m):
+    table[0][i] = 1
+
+  for i in range(1, n + 1):
+    print(table)
+    for j in range(m):
+      x = table[i - coins[j]][j] if i - coins[j] >= 0 else 0
+      y = table[i][j - 1] if j >= 1 else 0
+
+      table[i][j] = x + y
+
+  return table[n][m-1]
+
+if __name__ == "__main__":
+  coins = (1, 5, 10, 25)
+  print(waysToPay(coins, 40, 0))
+  print(waysToPayDP(coins, 40))
