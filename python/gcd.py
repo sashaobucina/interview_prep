9db8a52004693b054bf89015c292ebbfe2593899
Sasha Obucina <sasha.obucina@gmail.com>
2019-07-13 16:25:32 +0200

    Add efficient solution to find greatest common divisor

---
+++ python/gcd.py
@@ -0,0 +1,10 @@
+import math
+
+def gcd(a: int, b: int) -> int:
+  if b == 0:
+    return a
+  return gcd(b, a % b)
+
+if __name__ == "__main__":
+  a, b = 8, 4
+  assert gcd(a, b) == math.gcd(a, b)
