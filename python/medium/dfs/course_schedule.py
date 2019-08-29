c3a46bc29d0ab5065e9a4ff2a6bc3537ad72d65f
Sasha Obucina <sasha.obucina@gmail.com>
2019-08-28 20:46:26 -0400

    Add solution to course schedules II problem, topological sort with cycle detection

--- python/medium/dfs/course_schedule.py
+++ python/medium/dfs/course_schedule.py
@@ -41,6 +41,45 @@ def createGraph(prerequisites):
     graph[a].add(b)
   return graph
 
+"""
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+NOTE: Topological sort inspired algorithm
+"""
+def findOrder(numCourses: int, prerequisites: list) -> list:
+  graph = createGraph(prerequisites)
+  stk = []
+  visited = [False] * numCourses
+  recStack = [False] * numCourses
+
+  for i in range(numCourses):
+    if not visited[i]:
+      if topSort(i, graph, visited, recStack, stk):
+        return []
+  return stk
+
+
+def topSort(course, graph, visited, recStack, stack):
+  visited[course] = True
+  recStack[course] = True
+
+  for prereq in graph[course]:
+    if not visited[prereq]:
+      if topSort(prereq, graph, visited, recStack, stack):
+        return True
+    elif recStack[prereq]:
+      return True
+
+  stack.append(course)
+  recStack[course] = False
+  return False
+
 if __name__ == "__main__":
   # can finish
   prerequisites1 = [(1, 0)]
@@ -49,3 +88,10 @@ if __name__ == "__main__":
   # contains cycle, cant finish
   prerequisites2 = [(1, 0), (0, 1)]
   print(canFinish(2, prerequisites2))
+
+  print(findOrder(2, prerequisites1))
+
+  prerequisites3 = [(1, 0), (2, 0), (3, 1), (3, 2)]
+  print(findOrder(4, prerequisites3))
+
+  print(findOrder(2, prerequisites2))
