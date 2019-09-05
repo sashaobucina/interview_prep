c293d4609a750fc831db510fee5c72859fcf5e8d
Sasha Obucina <sasha.obucina@gmail.com>
2019-09-05 12:46:49 -0400

    Adding solution to finding kth largest element using min heapsO()

--- python/medium/kth_largest_element.py
+++ python/medium/kth_largest_element.py
@@ -1,12 +1,33 @@
 import heapq
 
+"""
+O(n + klogn)
+"""
 def kth_largest(arr: list, k: int) -> int:
-  heapq._heapify_max(arr)
+  copy = arr.copy()
+  heapq._heapify_max(copy)
   for i in range(k - 1):
-    heapq._heappop_max(arr)
-  return heapq._heappop_max(arr)
+    heapq._heappop_max(copy)
+  return heapq._heappop_max(copy)
+
+"""
+O(nlogk)
+"""
+def kth_largest2(arr: list, k: int) -> int:
+  subset = arr[:k].copy()
+  heapq.heapify(subset)
+  for i in range(k, len(arr)):
+    top = heapq.heappop(subset)
+    if arr[i] > top:
+      heapq.heappush(subset, arr[i])
+    else:
+      heapq.heappush(subset, top)
+
+  return heapq.heappop(subset)
+
 
 
 if __name__ == "__main__":
   arr = [7, 4, 6, 3, 9, 1]
-  print(kth_largest(arr, 2))
+  print(kth_largest(arr, 2))    # expected: 7
+  print(kth_largest2(arr, 2))   # expected: 7
