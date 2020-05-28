1b533db459c0caf94c73a1dbd7013d76dd27bc2a
Sasha Obucina <sasha.obucina@gmail.com>
2020-05-28 16:50:22 -0400

    Format #48; rotate matrix clcokwise

--- python/medium/rotate_image.py
+++ python/medium/rotate_image.py
@@ -18,29 +18,29 @@ def rotate_image(matrix: List[List[int]]) -> None:
 
 if __name__ == "__main__":
     matrix = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
     expected = [
-        [7,4,1],
-        [8,5,2],
-        [9,6,3]
+        [7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3]
     ]
     rotate_image(matrix)
     assert matrix == expected
 
     matrix = [
-        [ 5, 1, 9,11],
-        [ 2, 4, 8,10],
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
         [13, 3, 6, 7],
-        [15,14,12,16]
+        [15, 14, 12, 16]
     ]
     expected = [
-        [15,13, 2, 5],
+        [15, 13, 2, 5],
         [14, 3, 4, 1],
         [12, 6, 8, 9],
-        [16, 7,10,11]
+        [16, 7, 10, 11]
     ]
     rotate_image(matrix)
     assert matrix == expected
