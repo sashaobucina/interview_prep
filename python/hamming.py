0e6916419beeed04d23f65f57ea2c37b85340f8b
Sasha Obucina <sasha.obucina@gmail.com>
2020-06-01 00:32:20 -0400

    Solve #191; get hamming distance of integer

--- python/hamming.py
+++ python/hamming.py
@@ -8,7 +8,21 @@ def hamming_distance(x: int, y: int) -> int:
     return bin(x ^ y).count("1")
 
 
+def hamming_weight(n: int) -> int:
+    """
+    # 191: Write a function that takes an unsigned integer and return the number of '1' bits it has 
+    (also known as the Hamming weight).
+    """
+    count = 0
+    while n != 0:
+        n &= n - 1
+        count += 1
+
+    return count
+
+
 if __name__ == "__main__":
+    assert hamming_weight(11) == 3
     assert hamming_distance(1, 4) == 2
 
     print("Passed all tests!")
