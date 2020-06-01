4980a9d0448bf97562c5b017eef614fab120ee75
Sasha Obucina <sasha.obucina@gmail.com>
2020-05-31 20:31:39 -0400

    Solve #125; check if str is palindrome

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,35 +1,41 @@
+import re
+
+
 def is_palindrome(num: int) -> bool:
-  # Special cases
-  if (num < 0) or (num % 10 == 0 and num != 0):
-    return False
+    # Special cases
+    if (num < 0) or (num % 10 == 0 and num != 0):
+        return False
 
-  reverted_num = 0
-  while num > reverted_num:
-    reverted_num = (reverted_num * 10) + (num % 10)
-    num = num // 10
+    reverted_num = 0
+    while num > reverted_num:
+        reverted_num = (reverted_num * 10) + (num % 10)
+        num = num // 10
 
-  return num == reverted_num or num == reverted_num // 10
+    return num == reverted_num or num == reverted_num // 10
 
-"""
-Note: Ignore any non alphanumeric characters
-"""
+
 def isPalindrome(s: str) -> bool:
-  n = len(s) - 1
-  i = 0
-  while i < n:
-    if not s[i].isalnum():
-      i += 1
-      continue
-    if not s[n].isalnum():
-      n -= 1
-      continue
-    if s[i].lower() != s[n].lower():
-      print(s[i], s[n])
-      return False
-    i += 1
-    n -= 1
-  return True
+    """
+    # 125: Given a string, determine if it is a palindrome, considering only alphanumeric characters 
+    and ignoring cases.
+    """
+    s = re.sub(r"\W", "", s).lower()
+    start, end = 0, len(s) - 1
+
+    while start < end:
+        if s[start] != s[end]:
+            return False
+        start += 1
+        end -= 1
+
+    return True
+
 
 if __name__ == "__main__":
-  print(is_palindrome(123))
-  print(isPalindrome("A man, a plan, a canal: Panama"))
+    assert not is_palindrome(123)
+    assert is_palindrome(424)
+
+    assert isPalindrome("A man, a plan, a canal: Panama")
+    assert not isPalindrome("race a car")
+
+    print("Passed all tests!")
