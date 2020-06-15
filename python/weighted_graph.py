985b1cc60c808cdf4ca269288f928117185082d0
Sasha Obucina <sasha.obucina@gmail.com>
2020-06-15 13:32:27 -0400

    Add another impl of dijkstras algorithm

--- python/weighted_graph.py
+++ python/weighted_graph.py
@@ -1,3 +1,5 @@
+import heapq
+from typing import List, Tuple
 from collections import defaultdict, deque
 
 inf = float('inf')
@@ -51,17 +53,68 @@ class WeightedGraph:
             end = previous[end]
         if path:
             path.appendleft(end)
-        return list(path), distance[dest]
+        return distance[dest], list(path)
+
+
+def dijkstras(edges: List[Tuple[int]], src: int, dst: int) -> int:
+    graph = defaultdict(dict)
+    for start, end, cost in edges:
+        graph[start][end] = cost
+
+    q = [(0, src, ())]
+    visited = set()
+    distances = {src: 0}
+
+    while q:
+        cost, node, path = heapq.heappop(q)
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+        path = (*path, node)
+
+        if node == dst:
+            return (cost, path)
+
+        for neighbor in graph[node]:
+            if neighbor in visited:
+                continue
+
+            next_cost = cost + graph[node][neighbor]
+            prev_cost = distances.get(neighbor, None)
+            if prev_cost is None or next_cost < prev_cost:
+                distances[neighbor] = next_cost
+                heapq.heappush(q, (next_cost, neighbor, path))
+
+    return inf
 
 
 if __name__ == "__main__":
+    print("=== Dijkstra's ===")
     vertices = ['a', 'b', 'c', 'd', 'e', 'f']
-    edges = [("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
-             ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
-             ("e", "f", 9)]
-
+    edges = [
+        ("a", "b", 7),
+        ("a", "c", 9),
+        ("a", "f", 14),
+        ("b", "c", 10),
+        ("b", "d", 15),
+        ("c", "d", 11),
+        ("c", "f", 2),
+        ("d", "e", 6),
+        ("e", "f", 9)
+    ]
     g = WeightedGraph(vertices)
     for edge in edges:
-        g.add_edge(*edge)
+        g.add_edge(*edge, undirected=False)
 
-    print(g.dijkstras("a", "e"))
+    cost, path = g.dijkstras("a", "e")
+    print(f"Cost: {cost}, Path: {' -> '.join(path)}")
+
+    cost2, path2 = dijkstras(edges, "a", "e")
+    print(f"Cost: {cost2}, Path: {' -> '.join(path2)}")
+
+    assert cost == cost2
+    assert list(path) == list(path2)
+
+    print("Passed all tests!")
