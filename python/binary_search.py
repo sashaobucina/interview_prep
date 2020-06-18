c840b52530faf8e81839066da894b59831b7104f
Sasha Obucina <sasha.obucina@gmail.com>
2020-06-17 23:35:17 -0400

    Solve #704; implement standard binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,13 +1,37 @@
-def binary_search(n: list, target: int) -> int:
-  lo, hi = 0, len(n)
-  while lo < hi:
-    mid = (lo + hi) // 2
-    if n[mid] < target:
-      lo = mid + 1
-    else:
-      hi = mid
-  return lo
+from typing import List
+
+
+def binary_search(nums: List[int], target: int) -> int:
+    """
+    # 704: Given a sorted (in ascending order) integer array nums of n elements and a target value, write 
+    a function to search target in nums. If target exists, then return its index, otherwise return -1.
+    """
+    lo, hi = 0, len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        if target > nums[mid]:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+
+    return -1
+
 
 if __name__ == "__main__":
-  arr = [1, 2, 3, 4, 5, 6]
-  print(binary_search(arr, 5))
+    nums = [1, 2, 3, 4, 5, 6]
+    assert binary_search(nums, target=5) == 4
+
+    nums = [-1, 0, 3, 5, 9, 12]
+    assert binary_search(nums, target=2) == -1
+
+    nums = [-1, 0, 3, 5, 9, 12]
+    assert binary_search(nums, target=-2) == -1
+
+    nums = [-1, 0, 3, 5, 9, 12]
+    assert binary_search(nums, target=13) == -1
+
+    print("Passed all tests!")
