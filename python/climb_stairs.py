5b858ffbafaa2e2e8579d95ab97c6f2a2afbfe5a
Sasha Obucina <sasha.obucina@gmail.com>
2020-06-22 23:14:16 -0400

    Solve #70; climbing stairs problem with 1, 2 increments

--- python/climb_stairs.py
+++ python/climb_stairs.py
@@ -1,15 +1,30 @@
 def climb_stairs(n: int) -> int:
-  if n < 1:
-    raise IndexError("Arguments must be positive!")
-  if n == 1:
-    return 1
-  ways = [0] * n
-  ways[0] = 1
-  ways[1] = 2
-  for i in range(2, n):
-    ways[i] = ways[i-1] + ways[i-2]
-  return ways[n-1]
+    """
+    # 70: You are climbing a stair case. It takes n steps to reach to the top.
+
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+    NOTE: Input must be positive.
+    """
+    if n == 1:
+        return 1
+
+    memo = [0] * n
+
+    # handle base cases
+    memo[0] = 1
+    memo[1] = 2
+
+    # start handling memoized DP cases
+    for i in range(2, n):
+        memo[i] = memo[i - 1] + memo[i - 2]
+
+    return memo[n - 1]
 
 
 if __name__ == "__main__":
-  print(climb_stairs(0))
+    assert climb_stairs(2) == 2
+
+    assert climb_stairs(3) == 3
+
+    print("Passed all tests!")
