cf865f833a47ec6d4d0c20909086e9d7e10c05a4
Sasha Obucina <sasha.obucina@gmail.com>
2020-06-22 23:13:02 -0400

    Solve #922; sort by parity II

--- python/easy/sort_by_parity.py
+++ python/easy/sort_by_parity.py
@@ -18,8 +18,33 @@ def sort_by_parity(A: List[int]) -> List[int]:
     return even + odd
 
 
+def sort_by_parity_II(A: List[int]) -> List[int]:
+    """
+    # 922: Given an array A of non-negative integers, half of the integers in A are odd, and half of 
+    the integers are even.
+
+    Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+
+    You may return any answer array that satisfies this condition.
+    """
+    j = 1
+    for i in range(0, len(A), 2):
+        if A[i] % 2 == 0:
+            continue
+
+        while A[j] % 2 == 1:
+            j += 2
+
+        A[i], A[j] = A[j], A[i]
+
+    return A
+
+
 if __name__ == "__main__":
     A = [3, 1, 2, 4]
     assert sort_by_parity(A) == [2, 4, 3, 1]
+
+    A = [4, 2, 5, 7]
+    assert sort_by_parity_II(A) == [4, 5, 2, 7]
 
     print("Passed all tests!")
