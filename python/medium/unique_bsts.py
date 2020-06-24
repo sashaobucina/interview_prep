a48bb5e4f0c4a12cdcdb14d7b36deab608fa3bab
Sasha Obucina <sasha.obucina@gmail.com>
2020-06-24 12:54:48 -0400

    Solve #95; generate all structurally unique BSTs given an integer n

--- python/medium/unique_bsts.py
+++ python/medium/unique_bsts.py
@@ -1,3 +1,7 @@
+from typing import List
+from itertools import product
+
+
 def num_BSTs(n: int) -> int:
     """
     # 96: Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
@@ -14,9 +18,37 @@ def num_BSTs(n: int) -> int:
     return memo[n]
 
 
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def generate_BSTs(n: int) -> List[TreeNode]:
+    """
+    # 95: Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+    """
+    def _helper(m, n):
+        if m > n:
+            return [None]
+
+        ans = []
+        for i in range(m, n+1):
+            ls = _helper(m, i-1)
+            rs = _helper(i + 1, n)
+            for l, r in product(ls, rs):
+                ans.append(TreeNode(i, l, r))
+
+        return ans
+
+    if n == 0:
+        return []
+    return _helper(1, n)
+
+
 if __name__ == "__main__":
     expected = [1, 2, 5, 14, 42, 132, 429, 1430]
-
     for i in range(1, 8):
         assert num_BSTs(i) == expected[i - 1]
 
