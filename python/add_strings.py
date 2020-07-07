f95a0d9d6358a984f0e648b5db7dca7668257e92
Sasha Obucina <sasha.obucina@gmail.com>
2020-07-07 15:06:30 -0400

    Solve #415; add together two strings that represent nums

--- python/add_strings.py
+++ python/add_strings.py
@@ -1,30 +1,32 @@
-"""
-Given two non-negative integers num1 and num2 represented as string, return the sum of 
-num1 and num2.
-
-NOTE: You must not use any built-in BigInteger library or convert the inputs to integer directly.
-"""
 def add_strings(num1: str, num2: str) -> str:
-  i, j = len(num1) - 1, len(num2) - 1
-  carry = 0
-  sum = ""
+    """
+    # 415: Given two non-negative integers num1 and num2 represented as string, return the sum of 
+    num1 and num2.
 
-  while i >= 0 or j >= 0:
-    a = ord(num1[i]) - ord('0') if i >= 0 else 0
-    b = ord(num2[j]) - ord('0') if j >= 0 else 0
+    NOTE: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+    """
+    i, j = len(num1) - 1, len(num2) - 1
+    carry = 0
+    _sum = []
 
-    sum += str((a + b + carry) % 10)
-    carry = (a + b + carry) // 10
+    while i >= 0 or j >= 0:
+        a = ord(num1[i]) - ord("0") if i >= 0 else 0
+        b = ord(num2[j]) - ord("0") if j >= 0 else 0
 
-    i -= 1
-    j -= 1
+        _sum.append(str((a + b + carry) % 10))
+        carry = (a + b + carry) // 10
 
-  if carry:
-    sum += '1'
+        i -= 1
+        j -= 1
 
-  return sum[::-1]
+    if carry:
+        _sum.append("1")
+
+    return "".join(reversed(_sum))
+
 
 if __name__ == "__main__":
-  sol = add_strings("98", "9")
-  print(sol)   # expected: 107
-  print(type(sol) is str)    # expected: True
+    assert add_strings("98", "9") == "107"
+    assert add_strings("98", "0") == "98"
+
+    print("Passed all tests!")
