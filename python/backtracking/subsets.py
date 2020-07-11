a1b19fed99a3169953f9401b83fe2805f65e3ad6
Sasha Obucina <sasha.obucina@gmail.com>
2020-07-11 13:48:28 -0400

    Solve #78; find all unique subsets (order disregarded)

--- python/backtracking/subsets.py
+++ python/backtracking/subsets.py
@@ -1,44 +1,69 @@
-"""
-Given a set of distinct integers, return all possible subsets (the power set).
+from typing import List
 
-NOTE: Do not return any duplicate subsets
-"""
-def subsets_I(nums: list) -> list:
-  subsets = [[]]
-  for el in nums:
-    for i in range(len(subsets)):
-      currSubset = subsets[i]
-      subsets.append(currSubset + [el])
 
-  return subsets
+def subsets(nums: list) -> list:
+    """
+    # 78: Given a set of distinct integers, return all possible subsets (the power set).
 
-"""
-Given a list of integers that may contain duplicates, return all possible subsets (the power set).
+    NOTE: Do not return any duplicate subsets
+    """
+    subsets = [[]]
+    for num in nums:
+        for i in range(len(subsets)):
+            currSubset = subsets[i]
+            subsets.append(currSubset + [num])
 
-NOTE: Do not return any duplicate subsets
-"""
-def subsets_II(nums: list) -> list:
-  if not nums or len(nums) == 0:
-    return []
+    return subsets
 
-  nums = sorted(nums)
-  result = []
-  subset = []
-  toFindAllSubsets(nums, result, subset, 0)
-  return result
+
+def subsets_alt(nums: List[int]) -> List[List[int]]:
+    subsets = [[]]
+
+    def _helper(nums, acc):
+        subsets.append(acc)
+
+        for i, num in enumerate(nums):
+            _helper(nums[i+1:], acc + [num])
+
+    for i in range(len(nums)):
+        _helper(nums[i+1:], [nums[i]])
+
+    return subsets
+
+
+def subsets_II(nums: List[int]) -> List[List[int]]:
+    """
+    # 90: Given a list of integers that may contain duplicates, return all possible subsets (the power set).
+
+    NOTE: Do not return any duplicate subsets
+    """
+    if not nums or len(nums) == 0:
+        return []
+
+    nums = sorted(nums)
+    result = []
+    subset = []
+    toFindAllSubsets(nums, result, subset, 0)
+    return result
+
 
 def toFindAllSubsets(nums: list, result: list, subset: list, start: int) -> None:
-  result.append(subset.copy())
-  for i in range(start, len(nums)):
-    if (i != start and nums[i] == nums[i - 1]):
-      continue
-    subset.append(nums[i])
-    toFindAllSubsets(nums, result, subset, i + 1)
-    subset.pop()
+    result.append(subset.copy())
+    for i in range(start, len(nums)):
+        if (i != start and nums[i] == nums[i - 1]):
+            continue
+        subset.append(nums[i])
+        toFindAllSubsets(nums, result, subset, i + 1)
+        subset.pop()
 
 
 if __name__ == "__main__":
-  arr = [1, 2, 3]
-  print(subsets_I(arr))
-  arr2 = [1, 2, 2]
-  print(subsets_II(arr2))
+    nums = [1, 2, 3]
+    expected = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+    assert subsets(nums) == expected
+
+    nums = [1, 2, 2]
+    expected = [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]
+    assert subsets_II(nums) == expected
+
+    print("Passed all tests!")
