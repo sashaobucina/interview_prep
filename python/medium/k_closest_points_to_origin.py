fb0719b45448b12d9e42b3f833bcdb6932eb3673
Sasha Obucina <sasha.obucina@gmail.com>
2020-07-14 22:11:48 -0400

    Solve #973; K closest points from the origin using heap (Amazon)

---
+++ python/medium/k_closest_points_to_origin.py
@@ -0,0 +1,27 @@
+import heapq
+from typing import List
+
+
+def k_closest(points: List[List[int]], K: int) -> List[List[int]]:
+    """
+    # 973: We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+    (Here, the distance between two points on a plane is the Euclidean distance.)
+
+    You may return the answer in any order.  The answer is guaranteed to be unique 
+    (except for the order that it is in.)
+    """
+    h = []
+    for p in points:
+        dist = p[0] ** 2 + p[1] ** 2
+        h.append((dist, p))
+
+    k_smallest = heapq.nsmallest(K, h)
+    return [p for _, p in k_smallest]
+
+
+if __name__ == "__main__":
+    points = [[1, 3], [-2, 2]]
+    assert k_closest(points, 1) == [[-2, 2]]
+
+    print("Passed all tests!")
