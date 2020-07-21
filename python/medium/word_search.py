ae5f6cd751dd91b36217f7a42204fffdbd48227e
Sasha Obucina <sasha.obucina@gmail.com>
2020-07-21 12:31:41 -0400

    Solve #79; word search in 2-D grid using backtracking (DFS)

--- python/medium/word_search.py
+++ python/medium/word_search.py
@@ -1,53 +1,54 @@
-import copy
+from typing import List
 
-"""
-Given a 2D board and a word, find if the word exists in the grid.
 
-The word can be constructed from letters of a sequentially adjacent cell, where
-"adjacent" cells are those horizontally or vertically neighbouring. The same 
-letter cell cannot be used more than once.
-"""
-def wordSearch(grid: list, word: str) -> bool:
-  if not grid or len(grid) == 0 or len(grid[0]) == 0:
+def word_search(board: List[List[int]], word: str) -> bool:
+    """
+    # 79: Given a 2D board and a word, find if the word exists in the grid.
+
+    The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are 
+    those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+    This sol'n uses backtracking (DFS) to search for the word.
+    """
+    m, n = len(board), len(board[0])
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def dfs(word, x, y):
+        if word == "":
+            return True
+
+        ret_val = False
+        if 0 <= x < m and 0 <= y < n:
+            ch, rest = word[0], word[1:]
+            if board[x][y] != ch:
+                return False
+
+            board[x][y] = "#"
+            for dx, dy in directions:
+                if dfs(rest, x + dx, y + dy):
+                    ret_val = True
+                    break
+
+            board[x][y] = ch
+
+        return ret_val
+
+    for i in range(m):
+        for j in range(n):
+            if dfs(word, i, j):
+                return True
+
     return False
 
-  m, n = len(grid), len(grid[0])
-  for i in range(m):
-    for j in range(n):
-      if dfs(grid, word, i, j, 0):
-        return True
-  return False
-
-def dfs(grid: list, word: str, i: int, j: int, k: int) -> bool:
-  if grid[i][j] != word[k]:
-    return False
-
-  if k >= len(word) - 1:
-    return True
-
-  d_i = [-1, 0, 1, 0]
-  d_j = [0, 1, 0, -1]
-  char = grid[i][j]
-  grid[i][j] = '#'
-
-  for m in range(4):
-    p_i = i + d_i[m]
-    p_j = j + d_j[m]
-    if p_i >= 0 and p_i < len(grid) and p_j >= 0 and p_j < len(grid[0]) \
-                and grid[p_i][p_j] == word[k + 1]:
-      if dfs(grid, word, p_i, p_j, k + 1):
-        grid[i][j] = char
-        return True
-
-  grid[i][j] = char
-  return False
 
 if __name__ == "__main__":
-  matrix = [
-    ['A', 'B', 'C', 'E'],
-    ['S', 'F', 'C', 'S'],
-    ['A', 'D', 'E', 'E']
-  ]
-  print(wordSearch(matrix, "ABCCED"))   # True
-  print(wordSearch(matrix, "SEE"))   # True
-  print(wordSearch(matrix, "ABCB"))   # False
+    board = [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E']
+    ]
+    assert word_search(board, "ABCCED")
+    assert word_search(board, "SEE")
+    assert not word_search(board, "ABCB")
+
+    print("Passed all tests!")
