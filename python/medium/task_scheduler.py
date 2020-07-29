24e64256f2d6ff72f0e8bc75b634f9299a558c1f
Sasha Obucina <sasha.obucina@gmail.com>
2020-07-29 00:31:49 -0400

    Solve #621; CPU task scheduler using pure greedy approach

--- python/medium/task_scheduler.py
+++ python/medium/task_scheduler.py
@@ -3,7 +3,7 @@ from typing import List
 from collections import Counter, deque
 
 
-def least_interval(tasks: List[int], n: int) -> int:
+def least_interval(tasks: List[str], n: int) -> int:
     """
     # 621: You are given a char array representing tasks CPU need to do. It contains capital letters A to Z 
     where each letter represents a different task. Tasks could be done without the original order of the array.
@@ -15,6 +15,9 @@ def least_interval(tasks: List[int], n: int) -> int:
     (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
 
     You need to return the least number of units of times that the CPU will take to finish all the given tasks.
+
+    Time complexity: O(nlogn)
+    Space complexity: O(n)
     """
     if n == 0:
         return len(tasks)
@@ -49,14 +52,40 @@ def least_interval(tasks: List[int], n: int) -> int:
     return ans
 
 
+def least_interval_linear(tasks: List[str], n: int) -> int:
+    """
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+    # get the frequencies of the task
+    frequencies = [0] * 26
+    for task in tasks:
+        frequencies[ord(task) - ord("A")] += 1
+
+    frequencies.sort()
+
+    # max frequency
+    f_max = frequencies.pop()
+    idle_time = (f_max - 1) * n
+
+    while frequencies and idle_time > 0:
+        idle_time -= min(f_max - 1, frequencies.pop())
+    idle_time = max(0, idle_time)
+
+    return idle_time + len(tasks)
+
+
 if __name__ == "__main__":
     tasks = ["A", "A", "A", "B", "B", "B"]
     assert least_interval(tasks, 2) == 8
+    assert least_interval_linear(tasks, 2) == 8
 
     tasks = ["A", "A", "A", "B", "B", "B"]
     assert least_interval(tasks, 0) == 6
+    assert least_interval_linear(tasks, 0) == 6
 
     tasks = ["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"]
     assert least_interval(tasks, 2) == 16
+    assert least_interval_linear(tasks, 2) == 16
 
     print("Passed all tests!")
