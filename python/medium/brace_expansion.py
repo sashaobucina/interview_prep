5a26d6badd0517bcadece76881c5e2f5465a6eea
Sasha Obucina <sasha.obucina@gmail.com>
2020-08-10 22:33:24 -0400

    Solve #1087; brace expansion using backtracking, DFS

--- python/medium/brace_expansion.py
+++ python/medium/brace_expansion.py
@@ -1,38 +1,43 @@
-"""
-A string S represents a list of words.
+from typing import List
 
-Each letter in the word has 1 or more options.  If there is one option, the letter 
-is represented as is.  If there is more than one option, then curly braces delimit the options. 
-For example, "{a,b,c}" represents options ["a", "b", "c"].
 
-For example, "{a,b,c}d{e,f}" represents the list ["ade", "adf", "bde", "bdf", "cde", "cdf"].
+def expand(S: str) -> List[str]:
+    """
+    # 1087: A string S represents a list of words.
 
-Return all words that can be formed in this manner, in lexicographical order.
-"""
-def expand(S: str) -> list:
-  res = []
-  # N = sum(1 for s in S if s.isalpha())
+    Each letter in the word has 1 or more options.  If there is one option, the letter 
+    is represented as is.  If there is more than one option, then curly braces delimit the options. 
+    For example, "{a,b,c}" represents options ["a", "b", "c"].
 
-  def dfs(word: list, start: int):
-    if start == len(S):
-      res.append("".join(word))
-      return
+    For example, "{a,b,c}d{e,f}" represents the list ["ade", "adf", "bde", "bdf", "cde", "cdf"].
 
-    if S[start] != '{':
-      dfs(word + [S[start]], start + 1)
-    else:
-      letters = []
-      while S[start] != "}":
-        if S[start].isalpha():
-          letters.append(S[start])
-        start += 1
+    Return all words that can be formed in this manner, in lexicographical order.
+    """
+    res = []
 
-      letters.sort()
-      for letter in letters:
-        dfs(word + [letter], start + 1)
+    def dfs(word: str, i: int) -> None:
+        if i == len(S):
+            res.append(word)
+            return
 
-  dfs([], 0)
-  return res
+        if S[i] != "{":
+            dfs(word + S[i], i + 1)
+        else:
+            letters = []
+            while S[i] != "}":
+                if S[i].isalpha():
+                    letters.append(S[i])
+                i += 1
+
+            for letter in sorted(letters):
+                dfs(word + letter, i + 1)
+
+    dfs("", 0)
+    return res
+
 
 if __name__ == "__main__":
-  print(expand("{a,b}c{d,e}f"))   # expected: ["acdf","acef","bcdf","bcef"]
+    assert expand("{a,b}c{d,e}f") == ["acdf", "acef", "bcdf", "bcef"]
+    assert expand("abcd") == ["abcd"]
+
+    print("Passed all tests!")
