b0399aa1528705ebff70fc45139857d6b444bd87
Sasha Obucina <sasha.obucina@gmail.com>
2020-08-27 11:11:26 -0400

    Solve #436; find right interval using binary search

--- python/medium/find_right_interval.py
+++ python/medium/find_right_interval.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def find_right_intervals(intervals: List[int]) -> List[int]:
+def find_right_interval(intervals: List[int]) -> List[int]:
     """
     # 436: Given a set of intervals, for each of the interval i, check if there exists an interval j whose 
     start point is bigger than or equal to the end point of the interval i, which can be called that j 
@@ -15,6 +15,9 @@ def find_right_intervals(intervals: List[int]) -> List[int]:
     NOTE:
         - You may assume the interval's end point is always bigger than its start point.
         - You may assume none of these intervals have the same start point.
+
+    Time complexity: O(n * logn)
+    Space complexity: O(n)
     """
     N = len(intervals)
     res = [-1 for _ in range(N)]
@@ -51,17 +54,64 @@ def find_right_intervals(intervals: List[int]) -> List[int]:
     return res
 
 
+def find_right_interval_bin_search(intervals: List[int]) -> List[int]:
+    """
+    This sol'n use binary search instead of a double sort technique.
+
+    Time complexity: O(n * logn)
+    Space complexity: O(n)
+    """
+    def bin_search(target: int) -> List[int]:
+        l, r = 0, N - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if intervals[mid][0] >= target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return intervals[l] if l < N else [-1, -1]
+
+    N = len(intervals)
+    res = [-1 for _ in range(N)]
+
+    d = {}
+    for i, interval in enumerate(intervals):
+        start, end = interval
+        d[(start, end)] = i
+
+    # sort based on start times
+    intervals.sort(key=lambda x: x[0])
+
+    for start, end in intervals:
+
+        last_interval = bin_search(target=end)
+        last_start, last_end = last_interval
+
+        idx = d[(start, end)]
+        if last_interval != [-1, -1]:
+            last_idx = d[(last_start, last_end)]
+            res[idx] = last_idx
+
+    return res
+
+
 if __name__ == "__main__":
     intervals = [[1, 2]]
-    assert find_right_intervals(intervals) == [-1]
+    assert find_right_interval(intervals) == [-1]
+    assert find_right_interval_bin_search(intervals) == [-1]
 
     intervals = [[3, 4], [2, 3], [1, 2]]
-    assert find_right_intervals(intervals) == [-1, 0, 1]
+    assert find_right_interval(intervals) == [-1, 0, 1]
+    assert find_right_interval_bin_search(intervals) == [-1, 0, 1]
 
     intervals = [[1, 4], [2, 3], [3, 4]]
-    assert find_right_intervals(intervals) == [-1, 2, -1]
+    assert find_right_interval(intervals) == [-1, 2, -1]
+    assert find_right_interval_bin_search(intervals) == [-1, 2, -1]
 
     intervals = [[1, 12], [2, 9], [3, 10], [13, 14], [15, 16], [16, 17]]
-    assert find_right_intervals(intervals) == [3, 3, 3, 4, 5, -1]
+    assert find_right_interval(intervals) == [3, 3, 3, 4, 5, -1]
+    assert find_right_interval_bin_search(intervals) == [3, 3, 3, 4, 5, -1]
 
     print("Passed all tests!")
