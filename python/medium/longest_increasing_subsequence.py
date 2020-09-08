51c1cf8d3ddde222b287bc46171bcee6e1323f4b
Sasha Obucina <sasha.obucina@gmail.com>
2020-09-07 21:37:12 -0400

    Solve #300; longest increasing subsequence using DP

--- python/medium/longest_increasing_subsequence.py
+++ python/medium/longest_increasing_subsequence.py
@@ -1,24 +1,33 @@
-"""
-Longest increasing subsequence:
+from typing import List
 
-L[i] can be written recursively as:
-  L[i] = 1 + max(L[j]) where 0 < j < i and nums[j] < nums[i] ; or
-  L[i] = 1, if no such j exists
-"""
-def lis(nums: list) -> int:
-  n = len(nums)
-  table = [1] * n
 
-  for i in range(1, n):
-    for j in range(0, i):
-      if nums[i] > nums[j] and table[i] < table[j] + 1:
-        table[i] = table[j] + 1
+def lis(nums: List[int]) -> int:
+    """
+    # 300: Given an unsorted array of integers, find the length of longest increasing subsequence.
+    """
+    if not nums:
+        return 0
 
-  maximum = 0
-  for i in range(n):
-    maximum = max(maximum, table[i])
-  return maximum
+    N = len(nums)
+    dp = [0] * N
+
+    dp[0] = 1
+
+    ans = 1
+    for i in range(1, N):
+        curr_max = 0
+        for j in range(i):
+            if nums[i] > nums[j]:
+                curr_max = max(curr_max, dp[j])
+
+        dp[i] = curr_max + 1
+        ans = max(ans, dp[i])
+
+    return ans
+
 
 if __name__ == "__main__":
-  arr = [10, 9, 2, 5, 3, 7, 101, 18]
-  print(lis(arr))
+    arr = [10, 9, 2, 5, 3, 7, 101, 18]
+    assert lis(arr) == 4
+
+    print("Passed all tests!")
